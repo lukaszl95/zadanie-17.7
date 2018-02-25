2c8e70c2478b857699873bd3c6ffcc5bd9753e44
Lukasz Lucko <lukaszl95@o2.pl>
2018-02-25 12:50:57 +0100

    zadanie-17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '1082572807557-2m52obtd6n3dnrt95ed6pgol2jrs6lku.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'wC_Rn0s0dizSntqlPJSrbJRO',
+	'CALLBACK_URL': 'http://localhost:3001/auth/google/callback'
+};
